53aa9b343f1c96e08186090746dabaca03ce2d1d
Daniel Balabis <dkbalabis@gmail.com>
2022-05-29 10:39:58 -0700

    Set up express server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
+
+
+
